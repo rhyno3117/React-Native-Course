a5ef3cd7a381dab2522ca2f208fdadb006e344d2
ryan <rhynogolfer42@gmail.com>
2024-04-08 14:58:38 -0700

    Added comments for the primitive react elements of Text, Stylesheet

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet } from 'react-native'; //These are Primitive React Elements
 
 const ComponentsScreen = () => {
     return <Text style={styles.textStyle}>This is the components screen</Text>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native"; //These are Primitive React Elements
 
 const HomeScreen = () => {
   return <Text style={styles.text}>Hi there!</Text>;
