261e608566a90d46cd0ba5d21bf5793c40cc1b9a
ryan <rhynogolfer42@gmail.com>
2024-04-10 13:02:36 -0700

    2nd way to add key

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,23 +3,24 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        { name: 'Friend #1' , key: '1' },
-        { name: 'Friend #2' , key: '2'  },
-        { name: 'Friend #3' , key: '3'  },
-        { name: 'Friend #4' , key: '4' },
-        { name: 'Friend #5' , key: '5' },
-        { name: 'Friend #6' , key: '6' },
-        { name: 'Friend #7' , key: '7' },
-        { name: 'Friend #8' , key: '8' },
-        { name: 'Friend #9' , key: '9' },
-    ]
+        { name: 'Friend #1' },
+        { name: 'Friend #2' },
+        { name: 'Friend #3' },
+        { name: 'Friend #4' },
+        { name: 'Friend #5' },
+        { name: 'Friend #6' },
+        { name: 'Friend #7' },
+        { name: 'Friend #8' },
+        { name: 'Friend #9' }
+    ];
 
     return (
         <FlatList
+            keyExtractor={( friend ) => friend.name}
             data={friends}
             renderItem={({ item }) => {
                 // item === { name: 'Friend #1` }
-                return <Text>{ item.name }</Text>
+                return <Text>{item.name}</Text>;
             }}
         />
     );
