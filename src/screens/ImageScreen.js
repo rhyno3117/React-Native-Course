5eb151d13913be4e078a6ef8ca10219e81ee3331
ryan <rhynogolfer42@gmail.com>
2024-04-11 15:45:17 -0700

    updated

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,9 +5,18 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} />
-            <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} />
-            <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} />
+            <ImageDetail
+                title="Forest"
+                imageSource={require('../../assets/forest.jpg')}
+            />
+            <ImageDetail
+                title="Beach"
+                imageSource={require('../../assets/beach.jpg')}
+            />
+            <ImageDetail
+                title="Mountain"
+                imageSource={require('../../assets/mountain.jpg')}
+            />
         </View>
     )
 };
