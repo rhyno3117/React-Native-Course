ca057b2609667c8d86bb427e0ce1d2091b743e1f
ryan <rhynogolfer42@gmail.com>
2024-04-13 15:48:27 -0700

    updated

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
-    return <View>
-        <Text>Current Count:</Text>
-    </View>
+    return (
+        <View>
+            <Text>Current Count:</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});
