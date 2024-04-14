06c35fb1a1c38cecbcb473df030373fc47e12c19
ryan <rhynogolfer42@gmail.com>
2024-04-14 12:02:15 -0700

    Added CounterScreen component in App.js as well as a button in HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,6 +17,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Image Demo"
         onPress={() => navigation.navigate('Image')}
       />
+       <Button
+        title="Go to Counter Demo"
+        onPress={() => navigation.navigate('CounterScreen')}
+      />
       {/* <TouchableOpacity onPress={() => navigation.navigate('List')}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity> */}
