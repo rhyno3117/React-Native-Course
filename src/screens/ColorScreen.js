f905597ac60b3d08c2d2d191f636596ff603c54f
ryan <rhynogolfer42@gmail.com>
2024-04-15 22:30:59 -0700

    ColorScreen component is creating random colors for box

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -4,9 +4,18 @@ import { StyleSheet, View, Button } from "react-native";
 const ColorScreen = () => {
     return (
         <View>
-            <Button title="Add a Color"/>
+            <Button title="Add a Color" />
+            <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
         </View>
     );
+};
+
+const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red}, ${green}, ${blue})`;
 };
 
 const styles = StyleSheet.create({});
