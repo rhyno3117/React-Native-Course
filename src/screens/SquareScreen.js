063701f098d4dda43998447a35c502f0c8a35728
ryan <rhynogolfer42@gmail.com>
2024-04-16 15:48:34 -0700

    state variable created in parent component, passed callback function to child which updates value of state variable

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
+
+    console.log(red);
+
     return (
         <View>
-            <ColorCounter color="Red" />
-            <ColorCounter color="Blue" />
-            <ColorCounter color="Green" />
+            <ColorCounter
+                onIncrease={() => setRed(red + 1)}
+                onDecrease={() => setRed(red - 1)}
+                color="Red"
+            />
+            <ColorCounter
+                color="Blue"
+            />
+            <ColorCounter
+                color="Green"
+            />
         </View>
     )
 
