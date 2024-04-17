e6789241c4807b531d88789509aa0b2af764d4fe
ryan <rhynogolfer42@gmail.com>
2024-04-17 16:24:51 -0700

    added reducer that always returns new object as our state

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
+
+    const reducer = (state, action) => {
+        //state === {red: number, green: number, blue: number};
+        //action === {colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15};
+
+        switch (action.colorToChange) {
+            case 'red':
+                return { ...state, red: state.red + action.amount }
+            case 'green':
+                return { ...state, green: state.green + action.amount }
+            case 'blue':
+                return { ...state, blue: state.blue + action.amount }
+            default:
+                return state;
+        }
+    };
+
+
+
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+    // console.log(state) //{red:0, green:0, blue:0}
+
     return (
         <View>
             <ColorCounter
