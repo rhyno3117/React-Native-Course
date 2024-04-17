300453dffc6ca85cbebe5125084c7a09d11c0708
ryan <rhynogolfer42@gmail.com>
2024-04-17 12:52:00 -0700

    js code built for red color changer to not go past 255

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -9,23 +9,33 @@ const SquareScreen = () => {
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
 
-    console.log(red);
+    const setColor = (color, change) => {
+        // color === 'red', 'green', 'blue'
+        // change === +15, -15
+        if (color === 'red') {
+            if (red + change > 255 || red + change < 0) {
+                return;
+            } else {
+                setRed(red + change);
+            }
+        }
+    };
 
     return (
         <View>
             <ColorCounter
-                onIncrease={() => setRed(red + COLOR_INCREMENT)}
-                onDecrease={() => setRed(red - COLOR_INCREMENT)}
+                onIncrease={() => setColor('red', COLOR_INCREMENT)}
+                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
                 color="Red"
             />
             <ColorCounter
-            onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-            onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+                onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+                onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
                 color="Blue"
             />
             <ColorCounter
-            onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-            onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+                onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+                onDecrease={() => setGreen(green - COLOR_INCREMENT)}
                 color="Green"
             />
             <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
