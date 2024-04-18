f90c31e105d349fa85cefb53db5ec2ec136190f9
ryan <rhynogolfer42@gmail.com>
2024-04-18 15:58:39 -0700

    added BoxScreen.js inside of screens folder and it is fully connected with app

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import CounterScreen from "./src/screens/CounterScreen";
 import ColorScreen from "./src/screens/ColorScreen";
 import SquareScreen from "./src/screens/SquareScreen";
 import TextScreen from "./src/screens/TextScreen";
+import BoxScreen from "./src/screens/BoxScreen";
 
 const navigator = createStackNavigator(
   {
@@ -19,6 +20,7 @@ const navigator = createStackNavigator(
     Color: ColorScreen,
     Square: SquareScreen,
     Text: TextScreen,
+    Box: BoxScreen,
   },
   {
     initialRouteName: "Home",
---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const BoxScreen = () => {
+    return (
+        <View>
+            <Text>BoxScreen</Text>
+        </View>
+    );
+};
+const styles = StyleSheet.create({});
+
+export default BoxScreen
