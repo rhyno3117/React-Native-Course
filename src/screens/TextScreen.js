c0f18d12d9568ab64df24d0db74385f95189956d
ryan <rhynogolfer42@gmail.com>
2024-04-18 15:18:44 -0700

    added TextScreen.js inside of screens folder with boilder plate code

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const TextScreen = () => {
+  return (
+    <View>
+      <Text>TextScreen</Text>
+    </View>
+  )
+}
+
+const styles = StylesSheet.create({})
+
+export default TextScreen
