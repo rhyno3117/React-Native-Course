8d02c964b87e792f8c92a19d1129d672cf22460a
ryan <rhynogolfer42@gmail.com>
2024-04-18 15:26:23 -0700

    added TextInput and made text box more visible with styles

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import React from 'react'
 
+
 const TextScreen = () => {
-  return (
-    <View>
-      <Text>TextScreen</Text>
-    </View>
-  );
+    return (
+        <View>
+            <TextInput style={styles.input}/>
+        </View>
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+    }
+});
 
 export default TextScreen;
