19b00b6de7c61440c1bb707f9a5e4bab40ace75b
ryan <rhynogolfer42@gmail.com>
2024-04-18 16:27:25 -0700

    BoxScreen.js using flexbox now

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,21 +4,26 @@ import React from 'react'
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>BoxScreen</Text>
+            <Text style={styles.textStyle}>Child #1</Text>
+            <Text style={styles.textStyle}>Child #2</Text>
+            <Text style={styles.textStyle}>Child #3</Text>
         </View>
     );
 };
 const styles = StyleSheet.create({
     viewStyle:{
         borderWidth: 3,
-        borderColor: 'black'
+        borderColor: 'black',
+        // alignItems: 'center',
+        // alignItems: 'flex-end',
+        alignItems: 'flex-start'
     },
     textStyle:{
-        borderWidth: 10,
+        borderWidth: 3,
         borderColor: 'red',
         // marginVertical: 20,
         // marginHorizontal: 20,
-        margin: 20,
+        // margin: 20,
     }
 });
 
