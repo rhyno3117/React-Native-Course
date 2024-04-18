99f187c61a55fda0b2bea18f0c7fc19796800fd4
ryan <rhynogolfer42@gmail.com>
2024-04-17 17:25:03 -0700

    added CounterScreenDup.js which is a duplicate using useState. in the updated CounterScreen, I changed it so it is using a reducer instead of useState

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,26 +1,37 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+const reducer = (state, action) => {
+    //state === {count: number}
+    //action === {type: 'increment || 'decrement', payload: 1}
 
+    switch (action.type) {
+        case 'increment':
+            return { ...state, count: state.count + action.payload };
+        case 'decrement':
+            return { ...state, count: state.count - action.payload };
+        default:
+            return state;
+    }
+};
+
+const CounterScreen = () => {
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
     return (
         <View style={styles.container}>
             <Button
                 title="Increase"
                 onPress={() => {
-                    setCounter(counter + 1);
-                    // console.log(counter)
+                    dispatch({ type: 'increment', payload: 1 })
                 }}
             />
             <Button
                 title="Decrease"
                 onPress={() => {
-                    setCounter(counter - 1);
-                    // console.log(counter)
+                    dispatch({ type: 'decrement', payload: 1 })
                 }}
             />
-            <Text style={styles.counterText}>Current Count: {counter}</Text>
+            <Text style={styles.counterText}>Current Count: {state.count}</Text>
         </View>
     );
 };
@@ -30,7 +41,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     counterText: {
-        marginTop: 20, 
+        marginTop: 20,
     }
 });
 
---
+++ src/screens/CounterScreenDup.js
@@ -0,0 +1,37 @@
+// import React, { useState } from 'react';
+// import { View, Text, StyleSheet, Button } from 'react-native';
+
+// const CounterScreen = () => {
+//     const [counter, setCounter] = useState(0);
+
+//     return (
+//         <View style={styles.container}>
+//             <Button
+//                 title="Increase"
+//                 onPress={() => {
+//                     setCounter(counter + 1);
+//                     // console.log(counter)
+//                 }}
+//             />
+//             <Button
+//                 title="Decrease"
+//                 onPress={() => {
+//                     setCounter(counter - 1);
+//                     // console.log(counter)
+//                 }}
+//             />
+//             <Text style={styles.counterText}>Current Count: {counter}</Text>
+//         </View>
+//     );
+// };
+
+// const styles = StyleSheet.create({
+//     container: {
+//         alignItems: 'center',
+//     },
+//     counterText: {
+//         marginTop: 20, 
+//     }
+// });
+
+// export default CounterScreen;
